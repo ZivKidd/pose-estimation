31aeefde9ffb429af13cbe3760d39ce1bf5fa465
ykamikawa <ytkmkw@gmail.com>
2018-01-28 20:38:56 +0900

    refactor

--- pose_estimation.py
+++ pose_estimation.py
@@ -5,49 +5,55 @@ import argparse
 import cv2
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
-import scipy
 import math
 from tqdm import tqdm
 
 from paf import BuildModel
 import utils
-import computeAngle
+# import computeAngle
 
 
 # model config
-param = {'use_gpu': 1,
-     'GPUdeviceNumber': 0,
-     'modelID': '1',
-     'octave': 3,
-     'starting_range': 0.8,
-     'ending_range': 2.0,
-     'scale_search': [0.5, 1, 1.5, 2],
-     'thre1': 0.1,
-     'thre2': 0.05,
-     'thre3': 0.5,
-     'min_num': 4,
-     'mid_num': 10,
-     'crop_ratio': 2.5,
-     'bbox_ratio': 0.25
-    }
+param = {
+        'use_gpu': 1,
+        'GPUdeviceNumber': 0,
+        'modelID': '1',
+        'octave': 3,
+        'starting_range': 0.8,
+        'ending_range': 2.0,
+        'scale_search': [0.5, 1, 1.5, 2],
+        'thre1': 0.1,
+        'thre2': 0.05,
+        'thre3': 0.5,
+        'min_num': 4,
+        'mid_num': 10,
+        'crop_ratio': 2.5,
+        'bbox_ratio': 0.25}
 
 # model paraeters
 model_params = {'boxsize': 368,
                 'padValue': 128,
                 'np': '12',
                 'stride': 8,
-                'part_str': ['nose', 'neck', 'Rsho', 'Relb', 'Rwri', 'Lsho',
+                'part_str': [
+                    'nose', 'neck', 'Rsho', 'Relb', 'Rwri', 'Lsho',
                     'Lelb', 'Lwri', 'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne',
-                    'Lank', 'Leye', 'Reye', 'Lear', 'Rear', 'pt19]']}
+                    'Lank', 'Leye', 'Reye', 'Lear', 'Rear', 'pt19'
+                    ]}
 
 # find connection in the specified sequence, center 29 is in the position 15
-limbSeq = [[2,3], [2,6], [3,4], [4,5], [6,7], [7,8], [2,9], [9,10], \
-           [10,11], [2,12], [12,13], [13,14], [2,1], [1,15], [15,17], \
-           [1,16], [16,18], [3,17], [6,18]]
+limbSeq = [
+        [2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
+        [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17],
+        [1, 16], [16, 18], [3, 17], [6, 18]
+        ]
 # the middle joints heatmap correpondence
-mapIdx = [[31,32], [39,40], [33,34], [35,36], [41,42], [43,44], [19,20], [21,22], \
-          [23,24], [25,26], [27,28], [29,30], [47,48], [49,50], [53,54], [51,52], \
-          [55,56], [37,38], [45,46]]
+mapIdx = [
+        [31, 32], [39, 40], [33, 34], [35, 36], [41, 42], [43, 44], [19, 20],
+        [21, 22], [23, 24], [25, 26], [27, 28], [29, 30], [47, 48],
+        [49, 50], [53, 54], [51, 52], [55, 56], [37, 38], [45, 46]
+        ]
+
 
 # load image
 def loadimg(img_path):
@@ -55,31 +61,34 @@ def loadimg(img_path):
 
     return origimg
 
+
 # img -> input tensor
 def createinput(img, scale):
     # origimgのサイズにscaleをかける
-    imageToTest = cv2.resize(img, (0,0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+    imageToTest = cv2.resize(img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
     # 下と右をpadding padはどれだけpaddingしたか imageTo
     imageToTest_padded, pad = utils.padRightDownCorner(imageToTest, model_params['stride'], model_params['padValue'])
 
     return imageToTest_padded, pad
 
+
 # inference function
 def inferene(model, imageToTest_padded, pad):
     # 入力用へ (bacth, height, width, ch) channel last
-    input_img = np.transpose(imageToTest_padded[:,:,:,np.newaxis], (3,0,1,2))
+    input_img = np.transpose(imageToTest_padded[:, :, :, np.newaxis], (3, 0, 1, 2))
     # predicted outputs[0]paf and outputs[1]heatmap
     output_blobs = model.predict(input_img)
     # (1,h,w,ch) -> (h,w,ch)
     paf = np.squeeze(output_blobs[0])
     heatmap = np.squeeze(output_blobs[1])
     # resize maps size to padded inpus size
-    heatmap = cv2.resize(heatmap, (0,0), fx=model_params['stride'], fy=model_params['stride'], interpolation=cv2.INTER_CUBIC)
-    paf = cv2.resize(paf, (0,0), fx=model_params['stride'], fy=model_params['stride'], interpolation=cv2.INTER_CUBIC)
+    heatmap = cv2.resize(heatmap, (0, 0), fx=model_params['stride'], fy=model_params['stride'], interpolation=cv2.INTER_CUBIC)
+    paf = cv2.resize(paf, (0, 0), fx=model_params['stride'], fy=model_params['stride'], interpolation=cv2.INTER_CUBIC)
     heatmap = heatmap[:imageToTest_padded.shape[0]-pad[2], :imageToTest_padded.shape[1]-pad[3], :]
     paf = paf[:imageToTest_padded.shape[0]-pad[2], :imageToTest_padded.shape[1]-pad[3], :]
 
     return paf, heatmap
+
 
 # compute joints
 def compute_joint(heatmap, paf, origimg):
@@ -93,21 +102,27 @@ def compute_joint(heatmap, paf, origimg):
     special_k = []
     mid_num = 10
 
-    for part in range(19-1):
-        map_ori = heatmap[:,:,part]
+    for part in range(18):
+        map_ori = heatmap[:, :, part]
         map = gaussian_filter(map_ori, sigma=3)
 
         map_left = np.zeros(map.shape)
-        map_left[1:,:] = map[:-1,:]
+        map_left[1:, :] = map[:-1, :]
         map_right = np.zeros(map.shape)
-        map_right[:-1,:] = map[1:,:]
+        map_right[:-1, :] = map[1:, :]
         map_up = np.zeros(map.shape)
-        map_up[:,1:] = map[:,:-1]
+        map_up[:, 1:] = map[:, :-1]
         map_down = np.zeros(map.shape)
-        map_down[:,:-1] = map[:,1:]
+        map_down[:, :-1] = map[:, 1:]
 
-        peaks_binary = np.logical_and.reduce((map>=map_left, map>=map_right, map>=map_up, map>=map_down, map>param['thre1']))
-        peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0])) # note reverse
+        peaks_binary = np.logical_and.reduce((
+            map >= map_left,
+            map >= map_right,
+            map >= map_up,
+            map >= map_down,
+            map > param['thre1']))
+        # note reverse
+        peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]))
         peaks_with_score = [x + (map_ori[x[1],x[0]],) for x in peaks]
         id = range(peak_counter, peak_counter + len(peaks))
         peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
@@ -116,7 +131,7 @@ def compute_joint(heatmap, paf, origimg):
         peak_counter += len(peaks)
 
     for k in range(len(mapIdx)):
-        score_mid = paf[:,:,[x-19 for x in mapIdx[k]]]
+        score_mid = paf[:, :, [x-19 for x in mapIdx[k]]]
         candA = all_peaks[limbSeq[k][0]-1]
         candB = all_peaks[limbSeq[k][1]-1]
         nA = len(candA)
@@ -133,13 +148,15 @@ def compute_joint(heatmap, paf, origimg):
                         continue
                     vec = np.divide(vec, norm)
 
-                    startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
-                                   np.linspace(candA[i][1], candB[j][1], num=mid_num)))
+                    startend = list(
+                            zip(np.linspace(candA[i][0], candB[j][0], num=mid_num),
+                                np.linspace(candA[i][1], candB[j][1], num=mid_num))
+                            )
 
-                    vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
-                                      for I in range(len(startend))])
-                    vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
-                                      for I in range(len(startend))])
+                    vec_x = np.array([score_mid[int(round(startend[I][1])),
+                        int(round(startend[I][0])), 0] for I in range(len(startend))])
+                    vec_y = np.array([score_mid[int(round(startend[I][1])),
+                        int(round(startend[I][0])), 1] for I in range(len(startend))])
 
                     score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
                     score_with_dist_prior = sum(score_midpts)/len(score_midpts) + min(0.5*origimg.shape[0]/norm-1, 0)
@@ -149,10 +166,10 @@ def compute_joint(heatmap, paf, origimg):
                         connection_candidate.append([i, j, score_with_dist_prior, score_with_dist_prior+candA[i][2]+candB[j][2]])
 
             connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
-            connection = np.zeros((0,5))
+            connection = np.zeros((0, 5))
             for c in range(len(connection_candidate)):
-                i,j,s = connection_candidate[c][0:3]
-                if(i not in connection[:,3] and j not in connection[:,4]):
+                i, j, s = connection_candidate[c][0:3]
+                if(i not in connection[:, 3] and j not in connection[:, 4]):
                     connection = np.vstack([connection, [candA[i][3], candB[j][3], s, i, j]])
                     if(len(connection) >= min(nA, nB)):
                         break
@@ -170,14 +187,16 @@ def compute_joint(heatmap, paf, origimg):
 
     for k in range(len(mapIdx)):
         if k not in special_k:
-            partAs = connection_all[k][:,0]
-            partBs = connection_all[k][:,1]
+            partAs = connection_all[k][:, 0]
+            partBs = connection_all[k][:, 1]
             indexA, indexB = np.array(limbSeq[k]) - 1
 
-            for i in range(len(connection_all[k])): #= 1:size(temp,1)
+            # 1:size(temp,1)
+            for i in range(len(connection_all[k])):
                 found = 0
                 subset_idx = [-1, -1]
-                for j in range(len(subset)): #1:size(subset,1):
+                # 1:size(subset,1)
+                for j in range(len(subset)):
                     if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
                         subset_idx[found] = j
                         found += 1
@@ -188,16 +207,20 @@ def compute_joint(heatmap, paf, origimg):
                         subset[j][indexB] = partBs[i]
                         subset[j][-1] += 1
                         subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
-                elif found == 2: # if found 2 and disjoint, merge them
+
+                # if found 2 and disjoint, merge them
+                elif found == 2:
                     j1, j2 = subset_idx
                     # print ("found = 2")
-                    membership = ((subset[j1]>=0).astype(int) + (subset[j2]>=0).astype(int))[:-2]
-                    if len(np.nonzero(membership == 2)[0]) == 0: #merge
+                    membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
+
+                    # merge
+                    if len(np.nonzero(membership == 2)[0]) == 0:
                         subset[j1][:-2] += (subset[j2][:-2] + 1)
                         subset[j1][-2:] += subset[j2][-2:]
                         subset[j1][-2] += connection_all[k][i][2]
                         subset = np.delete(subset, j2, 0)
-                    else: # as like found == 1
+                    else:
                         subset[j1][indexB] = partBs[i]
                         subset[j1][-1] += 1
                         subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
@@ -208,11 +231,11 @@ def compute_joint(heatmap, paf, origimg):
                     row[indexA] = partAs[i]
                     row[indexB] = partBs[i]
                     row[-1] = 2
-                    row[-2] = sum(candidate[connection_all[k][i,:2].astype(int), 2]) + connection_all[k][i][2]
+                    row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
                     subset = np.vstack([subset, row])
 
     # delete some rows of subset which has few parts occur
-    deleteIdx = [];
+    deleteIdx = []
     for i in range(len(subset)):
         if subset[i][-1] < 4 or subset[i][-2]/subset[i][-1] < 0.4:
             deleteIdx.append(i)
@@ -220,24 +243,27 @@ def compute_joint(heatmap, paf, origimg):
 
     return subset, candidate
 
+
 # identify picher
 def create_dict(subset, candidate, oriImg):
     disperision = np.array([])
     for n in range(len(subset)):
-        #X_coords = np.array([])
+        # X_coords = np.array([])
         Y_coords = np.array([])
         for i in range(18):
             if subset[n][i] >= 0:
-                #X = candidate[subset[n][i], 0]
+                # X = candidate[subset[n][i], 0]
                 Y = candidate[subset[n][i], 1]
-                #X_coords = np.append(coords, X)
+                # X_coords = np.append(coords, X)
                 Y_coords = np.append(Y_coords, Y)
-        disperision = np.append(disperision,np.var(Y_coords))
+        disperision = np.append(disperision, np.var(Y_coords))
 
     person_id = np.argmax(disperision)
 
-    output = {"img_shape":(oriImg.shape[1], oriImg.shape[0]),
-          "joints":{}}
+    output = {
+            "img_shape": (oriImg.shape[1], oriImg.shape[0]),
+            "joints": {}
+            }
 
     for i in range(18):
         if subset[person_id][i] >= 0:
@@ -251,6 +277,7 @@ def create_dict(subset, candidate, oriImg):
 
     return output
 
+
 # dict to json
 def dict2json(output):
     json_str = json.dumps(output)
@@ -261,33 +288,39 @@ def dict2json(output):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="pose estimation model")
-    parser.add_argument("--target_path",
+    parser.add_argument(
+            "--target_path",
             "-t",
             default="/mnt/storage/clients/rakuten/Kobo/20170630_3P/01.mp4",
             type=str,
             help="target file path")
-    parser.add_argument("--weights_path",
+    parser.add_argument(
+            "--weights_path",
             "-w",
             default="./models/model.h5",
             type=str,
             help="weight path")
-    parser.add_argument("--output_dir",
+    parser.add_argument(
+            "--output_dir",
             "-o",
             default="./result/",
             type=str,
             help="output dirctory path")
-    parser.add_argument("--mode",
+    parser.add_argument(
+            "--mode",
             "-m",
             default="movie",
             choices=["movie", "image"],
             type=str,
             help="estimation mode")
-    parser.add_argument("--handedness",
+    parser.add_argument(
+            "--handedness",
             default="right",
             choices=["right", "left"],
             type=str,
             help="picher handedness")
-    parser.add_argument("--gpu_num",
+    parser.add_argument(
+            "--gpu_num",
             "-g",
             default="0",
             type=str,
@@ -298,8 +331,8 @@ if __name__ == "__main__":
 
     print("-----Loading weight-----")
     model = BuildModel(args.weights_path)
-    #print(model.summary())
-    print("---Done---")
+    # print(model.summary())
+    print("---Loading done---")
 
     if args.mode == "image":
         # load orignal image
@@ -337,7 +370,7 @@ if __name__ == "__main__":
             # get frame
             ret, origimg = cap.read()
             # cap flag
-            if ret == False:
+            if not ret:
                 break
             multiplier = [x * model_params["boxsize"] / origimg.shape[0] for x in param["scale_search"]]
             scale = multiplier[0]
@@ -359,7 +392,7 @@ if __name__ == "__main__":
             # output = angler.compute()
 
             # dict to json
-            output_path = args.output_dir+name+"/"+str("%06.f"%(frame_num+1))+".json"
+            output_path = args.output_dir + name + "/" + str("%06.f"%(frame_num+1)) + ".json"
             json_dict = dict2json(output)
 
             # json データの書き込み
